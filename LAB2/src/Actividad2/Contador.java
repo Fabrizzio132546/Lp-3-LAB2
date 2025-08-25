f7e4e67e2c392fb5c4ed37c73cb04bb368c1683a
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-25 10:05:31 -0500

    Update Contador.java
    agregando los metodos correspondientes

--- LAB2/src/Actividad2/Contador.java
+++ LAB2/src/Actividad2/Contador.java
@@ -1,3 +1,22 @@
 public class Contador {
     private int valor;
     static int acumulador = 0;
+    
+    public Contador(int valor) {
+        acumulador += valor;
+        this.valor = valor;
+    }
+
+    public void inc() {
+        valor++;
+        acumulador++;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public int getAcumulador() {
+        return acumulador;
+    }
+}
