fe032d466995160205dfcd0ddad25141f3aeae84
Fabrizzio <fbrzzochoa@gmail.com>
2025-08-25 08:32:13 -0500

    Agregando el programa del main

--- LAB2/src/Actividad4/Main.java
+++ LAB2/src/Actividad4/Main.java
@@ -1,10 +1,9 @@
 package Actividad4;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Persona fabrizzio = new Persona(1, "Fabrizzio", "Ochoa", 12345);
+        fabrizzio.getCuenta().setSaldo(2500.75);
+        System.out.println(fabrizzio);
+    }
 }
