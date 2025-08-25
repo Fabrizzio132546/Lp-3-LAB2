b735209facd948d7dcb7d10914673594e6e2ba51
Fabrizzio <fbrzzochoa@gmail.com>
2025-08-25 08:21:27 -0500

    subiendo main de la actividad 1

--- LAB2/src/Actividad1/Main.java
+++ LAB2/src/Actividad1/Main.java
@@ -3,9 +3,20 @@ package Actividad1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Coche coche1 = new Coche("camaro", 120, 20);
+		
+		coche1.encender();
+		coche1.acelerar();
+		coche1.frenar();
+		coche1.apagar();
+		
+		coche1.getModelo();
+		coche1.setModelo("Ford");
+		coche1.encender();
+		
+		Coche coche2 = new Coche("Toyota", "JR45", 2025, 123.0);
+		coche2.aplicarDescuento();
+		
 	}
-//hola mundo XD
-}
 
+}
