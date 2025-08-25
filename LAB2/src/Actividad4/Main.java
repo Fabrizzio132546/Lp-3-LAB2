fd0184ef07058608a7401463d424ed106972db7a
Fabrizzio <fbrzzochoa@gmail.com>
2025-08-25 08:33:57 -0500

    Update Main.java

--- LAB2/src/Actividad4/Main.java
+++ LAB2/src/Actividad4/Main.java
@@ -3,7 +3,11 @@ package Actividad4;
 public class Main {
     public static void main(String[] args) {
         Persona fabrizzio = new Persona(1, "Fabrizzio", "Ochoa", 12345);
+        Persona Lizardo = new Persona(2, "Lizardo", "Castillo", 98765);
         fabrizzio.getCuenta().setSaldo(2500.75);
         System.out.println(fabrizzio);
+        Lizardo.getCuenta().setSaldo(3462.5);
+        System.out.println(Lizardo);
     }
 }
+
