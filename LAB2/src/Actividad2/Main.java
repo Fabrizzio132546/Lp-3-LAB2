fa2d34a8a25ecd3599703e16e9dde5b80afe94c3
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-25 10:06:59 -0500

    Update Main.java
    agregando la clase y el metodo main

--- LAB2/src/Actividad2/Main.java
+++ LAB2/src/Actividad2/Main.java
@@ -1,10 +1,17 @@
 package Actividad2;
 
-public class Main {
+public class ContadorTest {
+	
+    public static void main(String[] args) {
+        Contador c1 = new Contador(3);
+        Contador c2 = new Contador(5);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        c1.inc();
+        c2.inc();
 
-	}
-
+        System.out.println("Valor c1: " + c1.getValor());
+        System.out.println("Valor c2: " + c2.getValor());
+        System.out.println("Acumulador: " + c1.getAcumulador());
+    }
 }
+
