056c852fd8d68c1cc772d221c4b6b451bc69bf5b
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-25 23:28:10 -0500

    Update cuenta.java
    clase cuenta

--- LAB2/src/Actividad5/cuenta.java
+++ LAB2/src/Actividad5/cuenta.java
@@ -1,1 +1,10 @@
+package Actividad5;
 
+public abstract class cuenta {
+    protected String numerocuenta;
+    protected double saldo;
+
+    public cuenta(String numerocuenta, double saldo) {
+        this.numerocuenta = numerocuenta;
+        this.saldo = saldo;
+    }
