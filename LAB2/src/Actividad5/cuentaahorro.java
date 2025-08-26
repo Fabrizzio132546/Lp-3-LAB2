fd19ad9e33b64248af103fe3f6e16db826e1ecae
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-25 23:37:12 -0500

    Update cuentaahorro.java

--- LAB2/src/Actividad5/cuentaahorro.java
+++ LAB2/src/Actividad5/cuentaahorro.java
@@ -1,17 +1,17 @@
 package Actividad5;
 
-public class cuentaahorro extends cuenta {
+public class cuentaahorro extends cuenta { 
     private double tasainteres;
     private double saldominimo;
 
     public cuentaahorro(String numerocuenta, double saldo, double tasainteres) {
         super(numerocuenta, saldo);
-        this.tasainteres = tasainteres;
+        this.tasainteres = tasainteres; //uso de this
         this.saldominimo = saldo;
     }
     
-    @Override
-    public void retirar(double monto) {
+    @Override 
+    public void retirar(double monto) { 
         super.retirar(monto);
         if (saldo < saldominimo) {
             saldominimo = saldo;
