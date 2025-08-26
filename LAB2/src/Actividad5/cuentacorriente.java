74b7ae864f83a7201d39dfe0ddfe540d7060453b
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-25 23:38:10 -0500

    Update cuentacorriente.java
    clase creada

--- LAB2/src/Actividad5/cuentacorriente.java
+++ LAB2/src/Actividad5/cuentacorriente.java
@@ -1,1 +1,11 @@
+package Actividad5;
 
+public class cuentacorriente extends cuenta {
+    private int retiros;
+    private static final int retiros_gratis = 3;
+    private static final double costo_retiro = 3.0;
+
+    public cuentacorriente(String numerocuenta, double saldo) {
+        super(numerocuenta, saldo);
+        this.retiros = 0;
+    }
