a7f963c8c2f46fd3e74399cf1bd1c527fc6c1ddd
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-25 23:34:50 -0500

    Update cuentaahorro.java
    crear la clase correspondiente a cuenta de ahorro

--- LAB2/src/Actividad5/cuentaahorro.java
+++ LAB2/src/Actividad5/cuentaahorro.java
@@ -1,1 +1,12 @@
+package Actividad5;
+
+public class cuentaahorro extends cuenta {
+    private double tasainteres;
+    private double saldominimo;
+
+    public cuentaahorro(String numerocuenta, double saldo, double tasainteres) {
+        super(numerocuenta, saldo);
+        this.tasainteres = tasainteres;
+        this.saldominimo = saldo;
+    }
 
