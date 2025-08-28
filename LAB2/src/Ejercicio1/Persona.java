074eeb162cd082d1263907162d36ab2d7cf5e063
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-27 20:41:35 -0500

    Update Persona.java

--- LAB2/src/Ejercicio1/Persona.java
+++ LAB2/src/Ejercicio1/Persona.java
@@ -4,3 +4,9 @@ public abstract class Persona {
     protected String nombre;
     protected String dni;
     protected int codigo;
+
+    public Persona(String n, String d, int c) {
+        this.nombre = n;
+        this.dni = d;
+        this.codigo = c;
+    }
