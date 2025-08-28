fc4a3d1959192ec631b67561baa55159f149cb7c
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-27 22:49:00 -0500

    Update Persona.java

--- LAB2/src/Ejercicio1/Persona.java
+++ LAB2/src/Ejercicio1/Persona.java
@@ -1,18 +1,18 @@
 package Ejercicio1;
 
-public abstract class Persona {
-    protected String nombre;
+public abstract class Persona { //clase abstracta que servira como base para otras clases 
+    protected String nombre; 
     protected String dni;
     protected int codigo;
 
-    public Persona(String n, String d, int c) {
+    public Persona(String n, String d, int c) { //contructor 
         this.nombre = n;
         this.dni = d;
         this.codigo = c;
     }
     
-    public abstract void info();
+    public abstract void info(); //metodo abstracto que las clases hijas deben implementar
 
-    public int getCodigo() { return codigo; }
-    public String getNombre() { return nombre; }
+    public int getCodigo() { return codigo; } //devuelve el codigo de la persona 
+    public String getNombre() { return nombre; } //devuelve el nombre de la persona 
 }
