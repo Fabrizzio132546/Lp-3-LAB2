6bde708be87b9e74d3428f08ef55dcc102e48c66
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-27 20:35:30 -0500

    Update Profesor.java
    clase profesor creado con los métodos solicitados

--- LAB2/src/Ejercicio1/Profesor.java
+++ LAB2/src/Ejercicio1/Profesor.java
@@ -1,1 +1,20 @@
+package Ejercicio1;
 
+public class Profesor extends Persona {
+    private String especialidad;
+    public static final int MaxCurso = 3;
+
+    public Profesor(String n, String d, int c, String esp) {
+        super(n, d, c);
+        this.especialidad = esp;
+    }
+
+    @Override
+    public void info() {
+        System.out.println("Profesor: " + nombre);
+        System.out.println("Codigo: " + codigo);
+        System.out.println("Especialidad: " + especialidad  + "\n" );
+    }
+
+    public String getEsp() { return especialidad; }
+}
