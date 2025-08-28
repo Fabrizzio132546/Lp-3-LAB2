ca6379ee41b0484e772c6684da59fcaaa706c168
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-27 22:58:07 -0500

    Update Profesor.java

--- LAB2/src/Ejercicio1/Profesor.java
+++ LAB2/src/Ejercicio1/Profesor.java
@@ -1,20 +1,20 @@
 package Ejercicio1;
 
-public class Profesor extends Persona { //CLASE PROFESORR
-    private String especialidad;
-    public static final int MaxCurso = 3;
+public class Profesor extends Persona { //clase profesor que hereda de perosna 
+    private String especialidad;  //atributo propio para profesor 
+    public static final int MaxCurso = 3; //numero maximo de cursos que dicta 
 
     public Profesor(String n, String d, int c, String esp) { //constructor 
-        super(n, d, c);
+        super(n, d, c);  // es del constructor persona 
         this.especialidad = esp;
     }
 
     @Override
-    public void info() {
+    public void info() { //implementa metodo abstracto info() de persona 
         System.out.println("Profesor: " + nombre);
         System.out.println("Codigo: " + codigo);
         System.out.println("Especialidad: " + especialidad  + "\n" );
     }
 
-    public String getEsp() { return especialidad; }
+    public String getEsp() { return especialidad; } //devuelve la especialidad  de profesor 
 }
