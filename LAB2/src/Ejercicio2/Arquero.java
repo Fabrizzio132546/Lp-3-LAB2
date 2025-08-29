e181dcfa85c31710bf375bb040092c7447110f2d
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-28 21:30:17 -0500

    Update Arquero.java

--- LAB2/src/Ejercicio2/Arquero.java
+++ LAB2/src/Ejercicio2/Arquero.java
@@ -1,1 +1,10 @@
+package Ejercicio2;
+
+public class Arquero extends Personaje implements IAtaqueFisico {
+    private Habilidad habilidad;
+
+    public Arquero(String nombre, int vida, int nivel, int ataque) {
+        super(nombre, vida, nivel, ataque);
+        this.habilidad = new Habilidad("Flecha Poderosa", 35);
+    }
 
