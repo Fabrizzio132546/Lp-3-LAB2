5515e6250e26b3e4694640727bd6efac6c5fd94a
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-28 21:30:30 -0500

    Update Arquero.java

--- LAB2/src/Ejercicio2/Arquero.java
+++ LAB2/src/Ejercicio2/Arquero.java
@@ -7,4 +7,17 @@ public class Arquero extends Personaje implements IAtaqueFisico {
         super(nombre, vida, nivel, ataque);
         this.habilidad = new Habilidad("Flecha Poderosa", 35);
     }
+    @Override
+    public void atacar(Personaje enemigo) {
+        System.out.println(nombre + " dispara una flecha contra " + enemigo.nombre);
+        enemigo.vida -= ataque;
+        if (enemigo.vida <= 0) enemigo.vida = 0;
+    }
+
+    @Override
+    public void usarHabilidad(Personaje enemigo) {
+        System.out.println(nombre + " usa " + habilidad.getNombre());
+        enemigo.vida -= habilidad.getPoder();
+        if (enemigo.vida <= 0) enemigo.vida = 0;
+    }
 
