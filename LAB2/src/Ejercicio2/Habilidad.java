e65292f948b1ec34120fb3e7c972a99fc47c9c63
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-28 21:31:46 -0500

    Update Habilidad.java

--- LAB2/src/Ejercicio2/Habilidad.java
+++ LAB2/src/Ejercicio2/Habilidad.java
@@ -3,3 +3,12 @@ package Ejercicio2;
 class Habilidad {
     private String nombre;
     private int poder;
+    
+    public Habilidad(String nombre, int poder) {
+        this.nombre = nombre;
+        this.poder = poder;
+    }
+
+    public String getNombre() { return nombre; }
+    public int getPoder() { return poder; }
+}
