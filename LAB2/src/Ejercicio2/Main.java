126ef936d58674576d96869299b2face1a700b53
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-08-28 21:26:42 -0500

    Update Main.java

--- LAB2/src/Ejercicio2/Main.java
+++ LAB2/src/Ejercicio2/Main.java
@@ -1,10 +1,13 @@
 package Ejercicio2;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
+        Personaje p1 = new Arquero("Fabrizzio", 140, 1, 30);
+        Personaje p2 = new Mago("Lizardo", 120, 1, 25);
 
-	}
-
-}
-
+        Personaje jugadorActual = null;
+        Personaje enemigo = null;
